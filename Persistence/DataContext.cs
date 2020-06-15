265643683fd0ebeace270da92392d0a87c045d80
Duncan Phelps <phelps.duncan@gmail.com>
2020-06-15 14:13:50 -0500

    basic skeleton started.

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+
+        //Values will be the table name
+        //we need to add this as a service to the app.
+        public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder){
+            builder.Entity<Value>()
+                .HasData(
+                    new Value {Id = 1, Name = "Value 101"},
+                    new Value { Id = 2, Name = "Value 102" },
+                    new Value { Id = 3, Name = "Value 103" }
+
+                );
+            
+        }
+    }
+}
